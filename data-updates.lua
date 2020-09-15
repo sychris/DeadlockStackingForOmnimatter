5f91bc87251fc4aee6ba0cde9f943bddcf6d4d26
Adrien 'Larandar' DUDOUIT-EXPOSITO <adrien@dudouit.me>
2020-09-15 20:01:03 +0200

    🔥 (Omnimatter): Make sure to use my icons if registered before

--- data-updates.lua
+++ data-updates.lua
@@ -8,10 +8,18 @@ local mods = {
 
 for _, mod_stackables in pairs(mods) do
     for _, item in pairs(mod_stackables.items) do
+        local stacked_name = "deadlock-stack-" .. item["item-name"]
+
+        if (not data.raw.item[stacked_name] == nil) then
+            print("DeadlockStacking for Omnimater: " ..
+                      "removing previously stacked item: " .. item["item-name"])
+            deadlock.destroy_stack(item["item-name"])
+        end
+
         -- item_name, graphic_path, target_tech, icon_size, item_type, mipmap_levels
         deadlock.add_stack(item["item-name"], item["icon"], item["unlock-tier"],
                            item["icon-size"])
-        local stacked_name = "deadlock-stack-" .. item["item-name"]
+
         for _, icon_addition in ipairs(item["icon-additions"]) do
             table.insert(data.raw.item[stacked_name].icons, icon_addition)
         end
